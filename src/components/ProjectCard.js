3447d66bd5a6aac70b22c1071fc15551d89e0d4a
aureasiqueira1 <aurea@gec.inatel.br>
2023-09-27 09:26:39 -0300

    feat: add project card

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,30 @@
+import { Col } from "react-bootstrap";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  padding: 10px;
+  width: 350px;
+  height: 350px;
+  margin-bottom: 24px;
+
+  background: #f1f1f1;
+  transition: 0.4s ease-in-out;
+`;
+
+export const ProjectCard = ({ title, description, imgUrl }) => {
+  return (
+    <Col size={12} sm={4} md={4}>
+      <div className="proj-imgbx">
+        <img src={imgUrl} />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+    </Col>
+  );
+};
