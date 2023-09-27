ae894d771edb28c687a655ae8747f6e0f07babbf
aureasiqueira1 <aurea@gec.inatel.br>
2023-09-27 09:27:56 -0300

    feat: add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Container, Row } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+        </Row>
+      </Container>
+    </footer>
+  );
+};
