ecbf1f8031bb7a59256f417dfe9c318f3064695e
aureasiqueira1 <aurea@gec.inatel.br>
2023-09-27 09:28:11 -0300

    feat: add download button

---
+++ src/components/DownloadButton.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { AiOutlineDownload } from "react-icons/ai";
+import pdf from "../public/Currículo_Áurea.pdf";
+
+const DownloadButton = () => {
+  const handleDownloadClick = () => {
+    // Substitua 'URL_DO_SEU_PDF' pela URL do seu arquivo PDF
+    const pdfUrl = pdf;
+
+    // Cria um elemento <a> temporário
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.target = "_blank";
+    link.download = "Currículo_Áurea.pdf"; // Defina o nome do arquivo como desejar
+
+    // Simula um clique no link para iniciar o download
+    link.click();
+  };
+
+  return (
+    <span className="curriculo" onClick={handleDownloadClick}>
+      <AiOutlineDownload />
+      CURRÍCULO
+    </span>
+  );
+};
+
+export default DownloadButton;
